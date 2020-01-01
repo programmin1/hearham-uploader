d0dfedaeae227135ada32126eec64ff859c3caa1
lb1programmer <luke@linuxpc>
2019-12-31 18:22:56 -0800

    More complete setup libs

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #Prerequisites
 echo "Downloading and compiling required files..."
-sudo apt-get install -y git build-essential zlib1g-dev libsdl2-dev libasound2-dev python3-gi pavucontrol
+sudo apt-get update
+sudo apt-get install -y git build-essential zlib1g-dev libsdl2-dev libasound2-dev python3-gi python3-pip sox pavucontrol
 wget https://sourceforge.net/projects/juliusmodels/files/ENVR-v5.4.Dnn.Bin.zip/download
 unzip download && rm ./download
 cp ./conf-additions/* ./ENVR-v5.4.Dnn.Bin/
